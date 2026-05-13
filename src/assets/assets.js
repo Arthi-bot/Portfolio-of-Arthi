d8ac46524ece71a18e214ca72c857b543d48c169
Arthi <seniorwebdeveloper12@gmail.com>
2026-05-13 14:31:50 +0530

    Resume and project details updated

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -6,6 +6,7 @@ import Logo_linkedin from './Logo_linkedin.png'
 import Project_1 from './Project_1.png'
 import Project_2 from './Project_2.png'
 import Project_3 from './Project_3.png'
+import resume from './Arthi_M_FD.pdf'
 
 const assets = {
     logo,
@@ -15,7 +16,8 @@ const assets = {
     Logo_linkedin,
     Project_1,
     Project_2,
-    Project_3
+    Project_3,
+    resume
 }
 
 export default assets;
