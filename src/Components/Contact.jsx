8024b2651a6ebc33d3a4c62dc5e6d745c2f74ef3
Arthi <seniorwebdeveloper12@gmail.com>
2026-05-24 15:55:38 +0530

    400 error solved

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,62 +1,44 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const Contact =()=>{
 
     const [status, setStatus] = useState(""); 
     const [loading, setLoading] = useState(false);
 
-      const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setStatus("");
 
-/*   const [status, setStatus] = useState(""); */
+        const formData = new FormData(e.target);
+        formData.append("access_key", "51b74d5f-1f5a-44e3-a25f-491df88718d1");
 
-  const handleChange = (e) => {
-    
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-   const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    setStatus("Sending...");
-
-    const res = await fetch("/api/solve", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-
-    const data = await res.json();
-
-    if (data.success) {
-      setStatus("success");
-      setTimeout(() => {
-        setFormData({
-          name: "",
-          email: "",
-          message: "",
+        try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
         });
-      }, 500);
-    } else {
-      setStatus("error");
-    }
-    setLoading(false);
-  };
 
-    useEffect(() => {
+        const data = await response.json();
+
+        if (data.success) {
+            setLoading(false);
+      setStatus("success");   // shows "Submitted"
+
         setTimeout(() => {
-                setStatus("");
-            }, 500);
-    }, [status]);
+            e.target.reset();     // clear form
+            setStatus("");        // back to "Submit"
+        }, 1000);
+            
+        } else {
+            setStatus("error");
+        }
+        } catch (error) {
+        setStatus("error");
+        }
 
+        setLoading(false);
+    };
     return(
         <div id="contact" className="max-w-full overflow-hidden py-20 md:py-28">
 
@@ -74,13 +56,13 @@ const Contact =()=>{
 
             <div className="px-4 lg:px-20 my-6 flex flex-col items-center">
                 
-                <form onSubmit={handleSubmit} className="w-full lg:w-3/5 bg-(--contact-bg-color) rounded-lg px-8 flex flex-col gap-4 py-6 justify-start">
+                <form onSubmit={onSubmit} className="w-full lg:w-3/5 bg-(--contact-bg-color) rounded-lg px-8 flex flex-col gap-4 py-6 justify-start">
                     <label htmlFor="name" className="text-(--secondary-text-color) text-lg tracking-wider">Name</label>
-                    <input name="name" type="text" required id="name" placeholder="Enter your name" className="bg-(--contact-box-color) w-full h-12 rounded-md border-0 outline-0 px-4 text-(--primary-text-color)" value={formData.name} onChange={handleChange}/>
+                    <input name="name" type="text" required id="name" placeholder="Enter your name" className="bg-(--contact-box-color) w-full h-12 rounded-md border-0 outline-0 px-4 text-(--primary-text-color)" />
                     <label htmlFor="email" className="text-(--secondary-text-color) text-lg tracking-wider">Email</label>
-                    <input name="email" type="email" required id="email" placeholder="Enter your Email" className="bg-(--contact-box-color) w-full h-12 rounded-md border-0 outline-0 px-4 text-(--primary-text-color)" value={formData.email} onChange={handleChange}/>
+                    <input name="email" type="email" required id="email" placeholder="Enter your Email" className="bg-(--contact-box-color) w-full h-12 rounded-md border-0 outline-0 px-4 text-(--primary-text-color)" />
                     <label htmlFor="message" className="text-(--secondary-text-color) text-lg tracking-wider">Message</label>
-                    <textarea name="message" required id="message" rows={8} placeholder="Enter your message" className="bg-(--contact-box-color) w-full rounded-md border-0 outline-0 px-4 py-4 text-(--primary-text-color)" value={formData.message} onChange={handleChange}></textarea> 
+                    <textarea name="message" required id="message" rows={8} placeholder="Enter your message" className="bg-(--contact-box-color) w-full rounded-md border-0 outline-0 px-4 py-4 text-(--primary-text-color)"></textarea> 
                     
                     <div className="w-full lg:w-[30%] bg-(--primary-color) cursor-pointer text-center py-3 rounded-md text-lg font-semibold tracking-wide text-(--primary-text-color) hover:bg-(--secondary-color) transition-colors duration-200 ease-in-out">
                         <button type="submit" disabled={loading} className="uppercase">
